1d7b64faae157ad91adde151e31ed6704f172621
Rajko Micic <rmicic@hotmail.com>
2023-04-06 22:28:38 +0000

    Modified the solution

--- Scripts/query1.sql
+++ Scripts/query1.sql
@@ -1,11 +1,13 @@
 SELECT
-e1.employeeId,
-e1.firstName,
-e1.lastName,
-e1.title,
-e2.firstName as managerFirstName,
-e2.lastName as managerLastName
+  e1.employeeId,
+  e1.firstName,
+  e1.lastName,
+  e1.title,
+  e2.firstName as managerFirstName,
+  e2.lastName as managerLastName
 FROM
-employee e1 join employee e2
-on e1.managerId = e2.employeeId
-WHERE e2.managerId is not NULL;
+  employee e1 
+  join employee e2
+  on e1.managerId = e2.employeeId
+WHERE 
+  e2.managerId is not NULL;
