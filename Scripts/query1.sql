61ecee09776099937cafe318145971d23ecc35d9
Rajko Micic <rmicic@hotmail.com>
2023-04-06 19:57:48 +0000

    Modifications for Solution 1

--- Scripts/query1.sql
+++ Scripts/query1.sql
@@ -1,17 +1,12 @@
+--Employees and their managers
 SELECT
 e1.employeeId,
 e1.firstName,
 e1.lastName,
+e1.title,
 e2.firstName as managerFirstName,
 e2.lastName as managerLastName
 FROM
 employee e1 join employee e2
 on e1.managerId = e2.employeeId
-WHERE e2.managerId is not NULL
-limit 10;
-
-GO
-
-SELECT * 
-FROM 
-employee limit 10;
+WHERE e2.managerId is not NULL;
